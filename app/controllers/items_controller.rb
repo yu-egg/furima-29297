22b51a9e85c2cd7650e37af8abd0268e8c0d01b7
yu-egg <69953750+yu-egg@users.noreply.github.com>
2020-09-18 11:27:03 +0900

    showアクションの定義

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -19,6 +19,10 @@ class ItemsController < ApplicationController
     end
   end
 
+  def show
+    @item - Item.find(params[:id])
+  end
+
   private
   def item_params
     params.require(:item).permit(:image,:name,:text,:category_id,:product_status_id,:presence_of_shipping_id,:shipping_origin_id,:date_of_shipment_id,:price).merge(user_id: current_user.id)
