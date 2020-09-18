b48ead285274c7cbdfed4bd2ca19c45059ddafd3
yu-egg <69953750+yu-egg@users.noreply.github.com>
2020-09-18 20:49:53 +0900

    商品編集機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -25,23 +25,23 @@ class ItemsController < ApplicationController
     @user = @item.user
   end
 
-  def edit
-    @item = Item.find(params[:id])
-  end
+    def edit
+      @item = Item.find(params[:id])
+    end
 
-  def update
-    @item = Item.find(params[:id])
-    if @item.user_id == current_user.id
-      if @item.update(item_params)
-        redirect_to item_path(@item.id)
+    def update
+      @item = Item.find(params[:id])
+      if @item.user_id == current_user.id
+        if @item.update(item_params)
+          redirect_to item_path(@item.id)
+        else
+          redirect_to action: :edit
+        end
       else
         redirect_to action: :edit
       end
-    else
-      redirect_to action: :edit
     end
-  end
-  
+
   private
   def item_params
     params.require(:item).permit(:image,:name,:text,:category_id,:product_status_id,:presence_of_shipping_id,:shipping_origin_id,:date_of_shipment_id,:price).merge(user_id: current_user.id)
