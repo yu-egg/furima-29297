71ff4b92db68bde0262f25f15c6bc8bba15298a6
yu-egg <69953750+yu-egg@users.noreply.github.com>
2020-09-18 20:28:44 +0900

    商品編集機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -28,6 +28,19 @@ class ItemsController < ApplicationController
   def edit
     @item = Item.find(params[:id])
   end
+
+  def update
+    @item = Item.find(params[:id])
+    if @item.user_id == current_user.id
+      if @item.update(item_params)
+        redirect_to item_path(@item.id)
+      else
+        redirect_to action: :edit
+      end
+    else
+      redirect_to action: :edit
+    end
+  end
   
   private
   def item_params
