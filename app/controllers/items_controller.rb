7c8c367c14525663ca4e934f8c534c87b1dbc81a
yu-egg <69953750+yu-egg@users.noreply.github.com>
2020-09-21 12:18:28 +0900

    修正版1

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -42,7 +42,12 @@ class ItemsController < ApplicationController
 
   def destroy
     @item.destroy
-    redirect_to items_path
+    if @item.destroy
+      redirect_to items_path
+    else
+      redirect_to item_path
+    end
+    
   end
 
   private
