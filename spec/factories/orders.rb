6ad76b557ba49f764b951b377e6447e96cb5a4aa
yu-egg <69953750+yu-egg@users.noreply.github.com>
2020-09-23 14:54:37 +0900

    テストコード

--- spec/factories/orders.rb
+++ spec/factories/orders.rb
@@ -1,5 +1,9 @@
 FactoryBot.define do
   factory :order do
-    
+    postal_code     {"123-4567"}
+    prefectures_id  {5}
+    city            {"豊島区池袋"}
+    address         {"4丁目"}
+    phone_number    {"12345678901"}
   end
 end
--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -1,5 +1,52 @@
 require 'rails_helper'
 
 RSpec.describe Order, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @order = FactoryBot.build(:order)
+  end
+  describe '商品購入' do
+
+    context '商品購入がうまくいくとき' do
+      it "クレジットカード情報が正しいとき購入できる" do
+        expect(@order).to be_valid
+      end
+      it "住所情報がもれなく記述されていると購入できる" do
+        expect(@order).to be_valid
+      end
+      it "電話番号が入力されていると購入できる" do
+        expect(@order).to be_valid
+      end
+    end
+
+    context '商品購入がうまくいかないとき' do
+
+      it "クレジットカード情報が空のときは購入できない" do
+      end
+      it "郵便番号が空のときは購入できない" do
+        @order.postal_code = nil
+        @order.valid?
+        expect(@order.errors.full_messages).to include("Postal code can't be blank")
+      end
+      it "都道府県が空のときは購入できない" do
+        @order.prefectures_id = nil
+        @order.valid?
+        expect(@order.errors.full_messages).to include("Prefectures can't be blank")
+      end
+      it "市区町村が空のときは購入できない" do
+        @order.city = nil
+        @order.valid?
+        expect(@order.errors.full_messages).to include("City can't be blank")
+      end
+      it "番地が空のときは購入できない" do
+        @order.address = nil
+        @order.valid?
+        expect(@order.errors.full_messages).to include("Address can't be blank")
+      end
+      it "電話番号が空のときは購入できない" do
+        @order.phone_number = nil
+        @order.valid?
+        expect(@order.errors.full_messages).to include("Phone number can't be blank")
+      end
+    end
+  end
 end
