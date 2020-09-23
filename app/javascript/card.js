70a4f24d583ef5005ff8c5f307e36216882fe6b3
yu-egg <69953750+yu-egg@users.noreply.github.com>
2020-09-23 12:46:27 +0900

    payjp導入

---
+++ app/javascript/card.js
@@ -0,0 +1,40 @@
+window.addEventListener("DOMContentLoaded", () => {
+  
+
+    Payjp.setPublicKey("pk_test_0c46ac3895bf4185a07315a8");
+    const form = document.getElementById("charge-form");
+    console.log(form)
+  
+    form.addEventListener("submit", (e) => {
+      e.preventDefault();
+      const formResult = document.getElementById("charge-form");
+      const formData = new FormData(formResult);
+      const card = {
+        number: formData.get("card-number"),
+        cvc: formData.get("card-cvc"),
+        exp_month: formData.get("card-exp-month"),
+        exp_year: `20${formData.get("card-exp-year")}`,
+      };
+      console.log(card)
+      
+      Payjp.createToken(card, (status, response) => {
+        console.log(status)
+        console.log(response.id)
+        if (status == 200) {
+          const token = response.id;
+          const renderDom = document.getElementById("charge-form");
+          const tokenObj = `<input value=${token} type="hidden" name='token'>`;
+          renderDom.insertAdjacentHTML("beforeend", tokenObj);
+        }
+        document.getElementById("card-number").removeAttribute("name");
+        document.getElementById("card-cvc").removeAttribute("name");
+        document.getElementById("card-exp-month").removeAttribute("name");
+        document.getElementById("card-exp-year").removeAttribute("name");
+        
+        document.getElementById("charge-form").submit();
+        document.getElementById("charge-form").reset();
+      })
+    })
+  })
+  // window.addEventListener("lord", pay);
+  // console.log("aaa")
--- db/migrate/20200921053326_create_orders.rb
+++ db/migrate/20200921053326_create_orders.rb
@@ -7,7 +7,7 @@ class CreateOrders < ActiveRecord::Migration[6.0]
       t.string     :address         ,null: false
       t.string     :building
       t.string     :phone_number    ,null: false
-      t.references :user_items      ,null: false, foreign_key: true
+      t.references :item            ,null: false, foreign_key: true
       t.timestamps
     end
   end
