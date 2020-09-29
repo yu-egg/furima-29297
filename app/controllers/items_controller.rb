92296141c3da9b9e7a885e6bc947e60dbd4dd995
yu-egg <69953750+yu-egg@users.noreply.github.com>
2020-09-29 19:03:57 +0900

    微調整

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,9 +1,10 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:show]
-  before_action :set_item, only: [:edit, :show, :update, :destroy]
+  before_action :set_item, only: [:edit, :show, :update, :destroy,]
 
   def index
     @items = Item.all.order("created_at DESC")
+    @item = Item.all
   end
   
   def new
