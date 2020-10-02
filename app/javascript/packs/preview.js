3f2df383bf3550b134d72d4354b11a7538651323
yu-egg <69953750+yu-egg@users.noreply.github.com>
2020-10-02 21:33:12 +0900

    生成したHTMLの要素をブラウザに表示

--- app/javascript/packs/preview.js
+++ app/javascript/packs/preview.js
@@ -7,5 +7,8 @@ document.addEventListener('DOMContentLoaded', function(){
     const imageElement = document.createElement('div');
     const blobImage = document.createElement('img');
     blobImage.setAttribute('src', blob);
+
+    imageElement.appendChild(blobImage);
+    ImageList.appendChild(imageElement);
   });
 });
